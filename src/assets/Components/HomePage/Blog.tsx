ac999631d233cbb256d23433ea97e3aae6361280
decagon <chiorlujack@gmail.com>
2023-07-20 12:42:38 +0100

    commit all

--- src/assets/Components/HomePage/Blog.tsx
+++ src/assets/Components/HomePage/Blog.tsx
@@ -3,45 +3,60 @@ import Footer from "../Footer/Footer"
 import Header from "../Header/Header"
 import { MainContent, UpcomingEvents } from "../Styled/Styled"
 import { navbar ,arr} from "./Contents"
+import { useState } from "react"
+interface BlogPost {
+    id: number;
+    post: string;
+    description: string;
+    category: string;
+    location: string;
+    organizer: string;
+    title: string;
+    image: string;
+  }
+  
+  interface Post {
+    data: BlogPost[];
+  }
+  
+  const Blog = () => {
+    const [datas, setData] = useState<Post>({ data: [] }); // Initialize datas as an object with an empty data array.
+  
+    fetch("http://localhost/prime/backend/RestApi/viewPost.php")
+      .then((response) => response.json())
+      .then((data: Post) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  
 
-
-const Blog =()=>{
 
     return (
         <>
         <Header navbar={navbar} />
+ 
         <UpcomingEvents>
-            <h1>OUR BLOG</h1>
-            <MainContent style={{
-                flexWrap:"wrap",justifyContent:"flex-start"
-            }}>
-                {arr.map((val,i)=>{
-                    return (
-                        <>
-                    <Box>
-                    <h1>{val.heading}</h1>
-                    <ImageDiv>
+    <h1>OUR BLOG</h1>
+    <MainContent style={{ flexWrap: "wrap", justifyContent: "flex-start" }}>
+    {datas.data.length < 1 ? <h1>No Blog Available</h1> : datas.data.map((val: BlogPost, i: number) => (
+            <Box key={i}>
+                <h1>{val.category}</h1>
+                <ImageDiv>
                     <img src={val.image} alt='' />
-                    <h3>&nbsp;&nbsp;Heading headf djfj</h3>
-                    <p style={
-                        {
-                            margin:"10px"
-                        }
-                    }>{val.para}</p>
-                    <p>&nbsp;&nbsp;&nbsp;&nbsp;Read More</p>
+                    <h3>Heading headf djfj</h3>
+                    <p style={{ margin: "10px" }}>{val.description}</p>
+                    <p>Read More</p>
                     <hr />
-                    <p>&nbsp;&nbsp;&nbsp;&nbsp;December 4, 2021</p>
-                    </ImageDiv>
-                   
-                    </Box>
-                        </>
-                    )
-                })}
+                    <p>December 4, 2021</p>
+                </ImageDiv>
+            </Box>
+        ))}
+    </MainContent>
+</UpcomingEvents>    
          
-         
-                
-            </MainContent>
-        </UpcomingEvents>
         <Footer />
         
         </>
